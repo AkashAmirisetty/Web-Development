fdbd169bcb170c628cf691954ac9a05f6307c9e2
AkashAmirisetty <amirisettyakash@gmail.com>
2025-03-28 18:02:01 +0530

    Files added

--- JavaScript/DOM/DOM Challenge Starting Files/index.js
+++ JavaScript/DOM/DOM Challenge Starting Files/index.js
@@ -1,1 +1,29 @@
-document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML = "Akash";
+// document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML = "Akash";
+// // Should give indexing as well which start from 0
+// document.getElementsByTagName("li")[2].style.color="green"
+// document.getElementsByClassName("btn")[0].style.color="blue"
+// // Used only for one element
+// document.getElementById("title").innerHTML="Good Bye"  
+
+
+// // document.querySelector("h1") --this can be used for one element
+
+// // document.querySelector("li .item") -- Can also used combiners
+
+// document.querySelectorAll("#list .item")[2].style.color="blue"
+// document.querySelector("li").style.color="red";
+
+document.getElementsByClassName("btn")[0].style.backgroundColor="yellow"
+
+
+
+
+
+
+
+
+
+
+
+
+
