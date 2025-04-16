2007ed7fe52a0d944d9936351a7728dd364113db
AkashAmirisetty <amirisettyakash@gmail.com>
2025-04-16 10:05:47 +0530

    Files added

--- Backend/2.3 NPM/index.js
+++ Backend/2.3 NPM/index.js
@@ -1,5 +1,13 @@
-var generateName=require("sillyname");
-var sillyName=generateName();
+// var generateName=require("sillyname");
+// var sillyName=generateName();
 
-console.log(`My name is ${sillyName}.`);
+// console.log(`My name is ${sillyName}.`);
+
+// var superhero=require("superheros");
+// var superHeroName=superhero();
+// console.log(`I am ${superHeroName}!`);
+
+const superheroes = require("superheroes");
+const name = superheroes.random();
+console.log(`I am ${name}!`);
 
